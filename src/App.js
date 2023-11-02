cfe7aa48915c71fd9191644f106406e25697884f
Ujala <ujala21@navgurukul.com>
2023-11-02 12:44:39 +0530

    UI

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import Note from "./noteApp/notes"
+function App() {
+  return (
+    <div className="App">
+        <Note/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/noteApp/notes.js
@@ -0,0 +1,12 @@
+const Note=()=>{
+    return(
+        <>
+            <h1>Notes</h1>
+            <input 
+                type="text"
+            />
+            <button>Add</button>
+        </>
+    )
+}
+export default Note;
